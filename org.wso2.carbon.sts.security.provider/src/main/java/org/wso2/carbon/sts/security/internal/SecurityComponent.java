24709618ef5cdd4e35573623f24396cf14a34a8b
Isuranga Perera <isurangamperera@gmail.com>
2017-08-28 00:17:28 +0530

    Policy building feature added

---
+++ org.wso2.carbon.sts.security.provider/src/main/java/org/wso2/carbon/sts/security/internal/SecurityComponent.java
@@ -0,0 +1,85 @@
+package org.wso2.carbon.sts.security.internal;
+
+import java.io.FileNotFoundException;
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.wso2.carbon.sts.security.provider.util.SecurityScenario;
+import org.wso2.carbon.sts.security.provider.util.SecurityScenarioDatabase;
+
+@Component(
+		name = "org.wso2.carbon.sts.security.component",
+		immediate = true
+)
+public class SecurityComponent {
+
+	XMLInputFactory factory = XMLInputFactory.newInstance();
+	
+	@Activate
+	public void start(BundleContext bundleContext) throws Exception {
+		XMLStreamReader streamReader = null;
+
+		URL resource = bundleContext.getBundle().getResource(
+				"/scenarios/scenario-config.xml");
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document doc = dBuilder.parse(resource.openStream());
+		doc.getDocumentElement().normalize();
+
+		NodeList nList = doc.getElementsByTagName("Scenario");
+
+		for (int temp = 0; temp < nList.getLength(); temp++) {
+			Node nNode = nList.item(temp);
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+				Element eElement = (Element) nNode;
+				String id = eElement.getAttribute("id");
+
+				// Need to clarify what to do with this case
+				if (id.equalsIgnoreCase("policyFromRegistry"))
+					continue;
+
+				SecurityScenario scenario = new SecurityScenario();
+
+				URL policyPath = bundleContext.getBundle().getResource(
+						"/scenarios/" + id + "-policy.xml");
+
+				try {
+					streamReader = factory.createXMLStreamReader(policyPath
+							.openStream());
+				} catch (FileNotFoundException | XMLStreamException e) {
+					e.printStackTrace();
+				}
+				
+				scenario.setPolicyStreamReader(streamReader);
+				scenario.setCategory(eElement.getElementsByTagName("Category")
+						.item(0).getTextContent());
+				scenario.setScenarioId(id);
+				scenario.setDescription(eElement
+						.getElementsByTagName("Description").item(0)
+						.getTextContent());
+				scenario.setSummary(eElement.getElementsByTagName("Summary")
+						.item(0).getTextContent());
+				scenario.setType(eElement.getElementsByTagName("Type").item(0)
+						.getTextContent());
+				scenario.setWsuId(eElement.getElementsByTagName("WsuId")
+						.item(0).getTextContent());
+				
+				SecurityScenarioDatabase.getInstance().put(id, scenario);
+			}
+		}
+	}
+	
+}
---
+++ org.wso2.carbon.sts.security.provider/src/main/java/org/wso2/carbon/sts/security/provider/SecurityPolicyServiceImpl.java
@@ -0,0 +1,29 @@
+package org.wso2.carbon.sts.security.provider;
+
+import javax.xml.stream.XMLStreamReader;
+
+import org.osgi.service.component.annotations.Component;
+import org.wso2.carbon.sts.security.provider.util.SecurityScenarioDatabase;
+
+@Component(
+		name = "org.wso2.carbon.sts.security.policy.service",
+		service = SecurityPolicyService.class,
+		immediate = true
+)
+public class SecurityPolicyServiceImpl implements SecurityPolicyService {
+
+	@Override
+	public void updateEffectivePolicy(String scenario) {
+		
+		//Update the security policy used by current tenant
+	}
+	
+	@Override
+	public XMLStreamReader getEffectivePolicy() {
+		
+		//Need to get policy using tenant information
+		/*String scenario = DataHolder.getInstance().getScenario();*/
+		String scenario = "UTOverTransport";
+		return SecurityScenarioDatabase.getInstance().getByWsuId(scenario).getPolicyStreamReader();
+	}
+}
